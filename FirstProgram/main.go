850a8b4f63fe480e1d7543a653eed499191cd63c
Ananul Haque <ananulhaque4631@gmail.com>
2026-03-29 11:21:43 +0600

    intro to Golang

---
+++ FirstProgram/main.go
@@ -0,0 +1,13 @@
+package main
+
+//must need package
+// main function package  name must be main
+
+import "fmt"
+
+// fmt=> format , built in package
+
+// func =function , main function
+func main() { 
+	fmt.Println("Hello world")
+}
---
+++ variable/variable.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+// data type
+
+/*
+1. numeric <1......n>
+1.1- integer number 10 <int> int, int8, int16, int32,int64, uint,uint8,uint16,uint32, uint64
+
+1.2-floating point number 10.5,13.124 <float> float32,or float64
+1.3-
+
+
+2.boolean <true,false> <bool>
+3.string <"a...z"> <string>
+4.
+
+*/
+
+func main() {
+	//**
+	// int
+	// float
+	// bool
+	// string
+	// */
+
+	// declare a initial variable
+	var x int = 10
+	var y float32 = 30.35
+	var age = 10
+	var isGo = true
+	var str = "Anamul"
+
+	a := 10
+	aa := true
+	aa = false
+	const num = 1100
+	fmt.Println(aa, x, y, age, isGo, str, num, a)
+	a = 100
+
+}
