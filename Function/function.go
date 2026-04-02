82b4ae5ea7d7eac75c5e69b016b3e4da3052b7cf
Ananul Haque <ananulhaque4631@gmail.com>
2026-04-02 07:06:16 +0600

    explore function

---
+++ Function/function.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+// func =function
+
+func arithmeticAction(num1 int, num2 int) {
+	sum := num1 + num2
+	sub := num1 - num2
+	multi := num1 * num2
+	div := num1 / num2
+	remainder := num1 % num2
+	fmt.Println(sum, sub, multi, div, remainder)
+}
+
+func addSomething(num1 int, num2 int, num3 int) int { // when you return function, obviously type of return
+
+	sum := num1 * num2 * num3
+
+	return sum
+
+}
+
+func multipleReturn(num1 int, num2 int) (int, int) {
+	sum := num1 + num2
+	mul := num1 * num2
+
+	return sum, mul
+}
+
+func main() {
+	// a := 3
+	// b := 12
+
+	// sum := (a + b) * 23
+	// fmt.Println(sum)
+
+	// arithmeticAction(120, 16)
+	// arithmeticAction(a, b)
+	// arithmeticAction(-10, 12)
+	// arithmeticAction(15, -23)
+	// arithmeticAction(-150, -23)
+
+	// sum := addSomething(12, 12, 12)
+	sum, mul := multipleReturn(14, 15)
+	fmt.Println(sum, mul)
+}
