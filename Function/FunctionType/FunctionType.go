fce846e69c7725c6db123bcd1e8aa0dfe21c9783
Ananul Haque <ananulhaque4631@gmail.com>
2026-04-11 17:52:45 +0600

    anonymous function

--- Function/FunctionType/FunctionType.go
+++ Function/FunctionType/FunctionType.go
@@ -2,24 +2,40 @@ package main
 
 import "fmt"
 
+var (
+	a = 10
+)
+
 // function type
 
-//1. Standard function or named function
+// 1. Standard function or named function
 func add() {
-	fmt.Println(4 + 6)
+	fmt.Println(4 + 16)
 }
 
 // 2. Anonymous function
 
-//3. function expression or Assign function in variable
+// func(a int, b int) {
+// 	c := a + b
+// 	fmt.Println(c)
+// }(4, 6)
+
+// 3. function expression or Assign function in variable
 
 // 4. Higher order function or first class function
 
 // 5. callback function
 
-//6. variadic function
+// 6. variadic function
 
-// 8. init function- you can not call this, computer call this automatically
+// 8. init function - you can not call this, computer call this automatically (at first call init function then  main then global and  others function)
+
+func init() {
+	fmt.Println("i am the first function that executed first")
+	fmt.Println(a)
+	a = 20
+
+}
 
 // 9. closure-close over
 
@@ -31,6 +47,15 @@ func add() {
 //
 
 func main() {
-	add()
+	// add()
+	// fmt.Println(a)
+
+	// anonymous function
+	// immediately invoked function expression,IIFE
+
+	func(a int, b int) {
+		c := a + b
+		fmt.Println(c)
+	}(4, 63)
 
 }
